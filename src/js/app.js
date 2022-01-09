7d03dd5c20c5c26759262659a533c69c02eef7a3
Stu <stuart.haddad@gmail.com>
2022-01-08 20:55:01 -0600

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,105 @@
+const THREE = window.THREE = require('three');
+require('three/examples/js/loaders/GLTFLoader');
+require('three/examples/js/controls/OrbitControls.js');
+
+import gsap from 'gsap';
+
+var camera, scene, renderer, controls, intersects, cube;
+const cubes = [];
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2(1, 1);
+const clock = new THREE.Clock();
+
+//Generate a random tag color
+let cr = Math.floor(Math.random() * 255);
+let cg = Math.floor(Math.random() * 255);
+let cb = Math.floor(Math.random() * 255);
+let tagColor = new THREE.Color(`rgb(${cr}, ${cg}, ${cb})`);
+
+init();
+animate();
+render();
+
+function init() {
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color('skyblue');
+  renderer = new THREE.WebGLRenderer({antialias: true});
+  const aspectRatio = window.innerWidth / window.innerHeight;
+  camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100);
+  camera.position.set(3, 6, 10);
+  // Add OrbitControls
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.autoRotate = true;
+  // controls.enableKeys = true;
+
+  // LIGHTS
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+  scene.add(ambientLight)
+
+  const cubeSpacing = 3;
+  // const numCubes = 9;
+  let delayCounter = 0;
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+  for (var i = -1; i <= 1; i++) {
+    for (var j = -1; j <= 1; j++) {
+      const material = new THREE.MeshBasicMaterial();
+      cube = new THREE.Mesh(geometry, material);
+      cubes.push(cube);
+      scene.add(cube);
+      cube.position.set(i * cubeSpacing, 0, j * cubeSpacing);
+      let delayTime = delayCounter;
+      gsap.to(cube.position, {duration: .3, delay: delayTime, y: 2})
+      delayCounter += .5;
+    }
+  }
+
+  window.addEventListener('mousemove', onMouseMove, false);
+  // renderer.outputEncoding = THREE.sRGBEncoding;
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+  console.log(cubes)
+}
+
+function render() {
+  // Update the picking ray with the camera and mouse position
+  raycaster.setFromCamera(mouse, camera);
+  // Get the objects in the scene being intersected
+  intersects = raycaster.intersectObjects(scene.children);
+  // Clear cube colors on every frame
+  for (let i = 0; i < cubes.length; i++) {
+    cubes[i].material.color.set("white");
+  }
+  if (intersects.length) {
+    for (let i = 0; i < intersects.length; i++) {
+      // Change all intersecting cubes to the same color 
+      intersects[i].object.material.color.set(tagColor);
+    }
+  }
+  renderer.render(scene, camera);
+}
+
+function animate() {
+
+  const elapsedTime = clock.getElapsedTime();
+  // cube.rotation.z += 0.01;
+
+  render();
+  requestAnimationFrame(animate);
+  controls.update();
+
+  // for (var i = 0; i < scene.children.length; i++) {
+  //   scene.children[i].position.y += 0.02
+  //   scene.children[i].rotation.y += 0.02;
+  // }
+
+}
+
+function onMouseMove(event) {
+  // calculate mouse position in normalized device coordinates
+  // (-1 to +1) for both components
+  event.preventDefault();
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+}
